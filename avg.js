a652045eca350caae8bf51dc9e6dfd60a3b9af5e
harshal upadhye <54963341+harshalupadhye@users.noreply.github.com>
2020-05-28 15:45:05 -0400

    Add files via upload
    this is a task 1 from day 7

---
+++ avg.js
@@ -0,0 +1,10 @@
+function find(){
+    var a=parseInt(document.calculate.num1.value);
+    var b=parseInt(document.calculate.num2.value);
+    var c=parseInt(document.calculate.num3.value);
+    var d=parseInt(document.calculate.num4.value);
+    var e=parseInt(document.calculate.num5.value);
+    var f=document.calculate.num6;
+    f.value=(a+b+c+d+e)/5;
+    
+}
